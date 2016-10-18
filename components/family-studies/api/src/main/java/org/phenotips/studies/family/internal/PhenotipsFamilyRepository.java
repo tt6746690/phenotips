5bebfc8ce38fd9eb24c4f337de9b88499458ba65
ItaiGershtansky <itaig.phenotips@gmail.com>
2016-10-18 16:32:27 -0400

    PT-2640-entities: Added getDocument() to return XWikiDocument. Changed all places that called getDocumentReference() and used bridge to get XWikiDocument.

--- components/family-studies/api/src/main/java/org/phenotips/studies/family/internal/PhenotipsFamilyRepository.java
+++ components/family-studies/api/src/main/java/org/phenotips/studies/family/internal/PhenotipsFamilyRepository.java
@@ -214,7 +214,7 @@ public class PhenotipsFamilyRepository implements FamilyRepository
             return null;
         }
         String patientId = patient.getId();
-        XWikiDocument patientDocument = getDocument(patient);
+        XWikiDocument patientDocument = patient.getDocument();
         if (patientDocument == null) {
             return null;
         }
@@ -260,7 +260,7 @@ public class PhenotipsFamilyRepository implements FamilyRepository
 
         String patientId = patient.getId();
         XWikiContext context = this.provider.get();
-        XWikiDocument patientDocument = getDocument(patient);
+        XWikiDocument patientDocument = patient.getDocument();
         if (patientDocument == null) {
             throw new PTInvalidPatientIdException(patientId);
         }
@@ -320,7 +320,7 @@ public class PhenotipsFamilyRepository implements FamilyRepository
 
         String patientId = patient.getId();
         XWikiContext context = this.provider.get();
-        XWikiDocument patientDocument = getDocument(patient);
+        XWikiDocument patientDocument = patient.getDocument();
         if (patientDocument == null) {
             throw new PTInvalidPatientIdException(patientId);
         }
@@ -773,19 +773,6 @@ public class PhenotipsFamilyRepository implements FamilyRepository
         }
         crtMaxID = Math.max(crtMaxID, 0);
         return crtMaxID;
-    }
-
-    private XWikiDocument getDocument(Patient patient)
-    {
-        // TODO use getDocument()
-        try {
-            DocumentReference document = patient.getDocumentReference();
-            XWikiDocument patientDocument = getDocument(document);
-            return patientDocument;
-        } catch (XWikiException ex) {
-            this.logger.error("Can't get patient document for patient [{}]: []", patient.getId(), ex);
-            return null;
-        }
     }
 
     private XWikiDocument getDocument(EntityReference docRef) throws XWikiException
